e372a99b4ca731c27853a7e232e58224e148df91
Jakub Szczechura <jakub.szczechura@execon.pl>
2022-11-16 01:20:55 +0100

    asd

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -26,6 +26,8 @@
 
             <input type='text' v-model="someText" />
             {{ someText }}
+
+            <a href="https://google.com/">Google</a>
             <button
               class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
               type="submit"
